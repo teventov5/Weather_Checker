56807677d35ba007afbfb46c5cc9b77b630e524d
teventov5 <teventov@gmail.com>
2020-07-09 14:06:39 +0300

    eclipse switch

--- WeatherApp/src/main/java/com/T_Y/controller/ApiKey.java
+++ WeatherApp/src/main/java/com/T_Y/controller/ApiKey.java
@@ -2,6 +2,6 @@ package com.T_Y.controller;
 
 public class ApiKey {
     public static String getApiKey() {
-        return "GkwJCojNt1j7y1RJLd8lGflzvBjiCZNU";
+        return "bA5RVGU0E68ykGtGrWL0NHHlXrPgfDgh";
     }
 }
